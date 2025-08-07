04cdbc71d1be465a85de28f80556bd05ed67b29c
Slava Osipov <55810653+osipov-slava@users.noreply.github.com>
2025-08-07 11:43:39 +0300

    add negative test

--- src/test/java/ru/job4j/devops/CalcApplicationTest.java
+++ src/test/java/ru/job4j/devops/CalcApplicationTest.java
@@ -2,6 +2,12 @@ package ru.job4j.devops;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
+import ru.job4j.devops.controllers.CalcController;
+import ru.job4j.devops.models.Result;
+import ru.job4j.devops.models.TwoArgs;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class CalcApplicationTests {
@@ -13,5 +19,14 @@ class CalcApplicationTests {
     @Test
     void mainMethodTest() {
         CalcApplication.main(new String[] {});
+    }
+
+    @Test
+    public void whenNegativeNumber() {
+        var input = new TwoArgs(-1, -1);
+        var expected = new Result(-2);
+        var output = new CalcController().summarise(input);
+        assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+        assertThat(output.getBody()).isEqualTo(expected);
     }
 }
